8dffcff5c92de6d791534fe3d8b7be49f0855c06
Alexey Filippov <afilippov@acm.org>
2022-12-06 05:08:02 +0000

    DRY

--- day_06.py
+++ day_06.py
@@ -8,18 +8,19 @@ def all_diff(s):
     return len(set(s)) == len(s)
 
 
-def part1(s):
-    for i in range(len(s) - 4):
-        if all_diff(s[i:i+4]):
-            return i + 4
+def part(s, n):
+    for i in range(len(s) - n):
+        if all_diff(s[i:i+n]):
+            return i + n
     return -1
+
+
+def part1(s):
+    return part(s, 4)
 
 
 def part2(s):
-    for i in range(len(s) - 14):
-        if all_diff(s[i:i+14]):
-            return i + 14
-    return -1
+    return part(s, 14)
 
 
 assert part1(example) == 10
