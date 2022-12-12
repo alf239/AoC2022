a236a0ee4b2d0e936fa11491fcbdbdbebd5ce6ec
Alexey Filippov <afilippov@acm.org>
2022-12-12 05:35:37 +0000

    Create day_12.py

---
+++ day_12.py
@@ -0,0 +1,116 @@
+import aoc
+import queue
+
+task = aoc.get_input(12)
+example = """Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi"""
+
+
+def read(m, x, y):
+    if x < 0: return '|'
+    if y < 0: return '|'
+    if x >= len(m[0]): return '|'
+    if y >= len(m): return '|'
+    signal = m[y][x]
+    if signal == 'S': return 'a'
+    if signal == 'E': return 'z'
+    return signal
+
+
+def start(m):
+    for y in range(len(m)):
+        for x in range(len(m[0])):
+            if m[y][x] == 'S':
+                return x, y
+    raise Exception("Cannot start")
+
+
+def finish(m):
+    for y in range(len(m)):
+        for x in range(len(m[0])):
+            if m[y][x] == 'E':
+                return x, y
+    raise Exception("Cannot start")
+
+
+def part1(ss):
+    m = [[c for c in s] for s in ss.splitlines()]
+    visited = {}
+    work = []
+    x0, y0 = start(m)
+    xz, yz = finish(m)
+    work[:0] = [(x0, y0, 0)]
+    while work:
+        x, y, cnt = work.pop()
+        if (x, y) in visited:
+            continue
+        visited[(x, y)] = cnt
+        signal = read(m, x, y)
+        if (x, y) == (xz, yz):
+            # for y in range(len(m)):
+            #     for x in range(len(m[0])):
+            #         if (x, y) in visited:
+            #             print(f'{visited[(x, y)]:3d}', end="")
+            #         else:
+            #             print("   ", end="")
+            #     print()
+            return cnt
+        if signal == 'S':
+            signal = 'a'
+        step = cnt + 1
+        if ord(read(m, x + 1, y)) <= ord(signal) + 1:
+            work[:0] = [(x + 1, y, step)]
+        if ord(read(m, x - 1, y)) <= ord(signal) + 1:
+            work[:0] = [(x - 1, y, step)]
+        if ord(read(m, x, y + 1)) <= ord(signal) + 1:
+            work[:0] = [(x, y + 1, step)]
+        if ord(read(m, x, y - 1)) <= ord(signal) + 1:
+            work[:0] = [(x, y - 1, step)]
+
+    raise Exception("Out of work, no cigar")
+
+
+def part2(ss):
+    m = [[c for c in s] for s in ss.splitlines()]
+    visited = {}
+    work = []
+    x0, y0 = finish(m)
+    work[:0] = [(x0, y0, 0)]
+    while work:
+        x, y, cnt = work.pop()
+        if (x, y) in visited:
+            continue
+        visited[(x, y)] = cnt
+        signal = read(m, x, y)
+        if signal == 'a':
+            # for y in range(len(m)):
+            #     for x in range(len(m[0])):
+            #         if (x, y) in visited:
+            #             print(f'{visited[(x, y)]:3d}', end="")
+            #         else:
+            #             print("   ", end="")
+            #     print()
+            return cnt
+        if signal == 'S':
+            signal = 'a'
+        step = cnt + 1
+        if ord(read(m, x + 1, y)) >= ord(signal) - 1:
+            work[:0] = [(x + 1, y, step)]
+        if ord(read(m, x - 1, y)) >= ord(signal) - 1:
+            work[:0] = [(x - 1, y, step)]
+        if ord(read(m, x, y + 1)) >= ord(signal) - 1:
+            work[:0] = [(x, y + 1, step)]
+        if ord(read(m, x, y - 1)) >= ord(signal) - 1:
+            work[:0] = [(x, y - 1, step)]
+
+    raise Exception("Out of work, no cigar")
+
+
+assert part1(example) == 31
+print("Part 1:", part1(task))
+
+assert part2(example) == 29
+print("Part 2:", part2(task))
