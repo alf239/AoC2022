0f5fb445883a45f091eedf8feb4e2eb6d302255c
Alexey Filippov <afilippov@acm.org>
2022-12-20 04:54:04 +0000

    Part 2, too

--- day_19.py
+++ day_19.py
@@ -1,6 +1,6 @@
 import dataclasses
+from collections import deque
 from typing import Tuple
-from functools import cache
 
 import aoc
 
@@ -50,75 +50,98 @@ def parse_input(inp):
     return [parse_blueprint(s) for s in inp.splitlines()]
 
 
-@cache
-def max_geodes(bp, ore, ore_bots, clay, clay_bots, obsidian, obsidian_bots, t):
-    if t == 0:
-        return 0
-    new_ore = ore + ore_bots
-    new_clay = clay + clay_bots
-    new_obsidian = obsidian + obsidian_bots
-    remaining_time = t - 1
-    if ore >= bp.geode[0] and obsidian >= bp.geode[1]:
-        return (remaining_time +
-                max_geodes(bp,
-                           new_ore - bp.geode[0], ore_bots,
-                           new_clay, clay_bots,
-                           new_obsidian - bp.geode[1], obsidian_bots,
-                           remaining_time))
+def max_geodes(bp, t):
+    work = deque()
+    seen = set()
+    baseline = {}
     best = 0
-    if ore >= bp.obsidian[0] and clay >= bp.obsidian[1]:
-        best = max(best,
-                   max_geodes(bp,
-                              new_ore - bp.obsidian[0], ore_bots,
-                              new_clay - bp.obsidian[1], clay_bots,
-                              new_obsidian, obsidian_bots + 1,
-                              remaining_time))
 
-    if ore >= bp.clay:
-        best = max(best,
-                   max_geodes(bp,
-                              new_ore - bp.clay, ore_bots,
-                              new_clay, clay_bots + 1,
-                              new_obsidian, obsidian_bots,
-                              remaining_time))
+    work.append((t,
+                 0, 1,
+                 0, 0,
+                 0, 0,
+                 0, 0))
+    while work:
+        state = work.pop()
+        if state in seen:
+            continue
+        seen.add(state)
+        t, ore, ore_bots, clay, clay_bots, obsidian, obsidian_bots, geode, geode_bots = state
+        remaining_time = t - 1
 
-    if ore >= bp.ore:
-        best = max(best,
-                   max_geodes(bp,
-                              new_ore - bp.ore, ore_bots + 1,
-                              new_clay, clay_bots,
-                              new_obsidian, obsidian_bots,
-                              remaining_time))
+        bl = baseline.setdefault(t, 0)
+        if (geode_bots + 1) * t + geode < bl:
+            continue
+        baseline[t] = max(bl, geode_bots * remaining_time + geode)
 
-    best = max(best, max_geodes(bp,
-                                new_ore, ore_bots,
-                                new_clay, clay_bots,
-                                new_obsidian, obsidian_bots,
-                                remaining_time))
+        new_ore = ore + ore_bots
+        new_clay = clay + clay_bots
+        new_obsidian = obsidian + obsidian_bots
+        new_geode = geode + geode_bots
+
+        if remaining_time == 0:
+            best = max(new_geode, best)
+            continue
+
+        if ore >= bp.geode[0] and obsidian >= bp.geode[1]:
+            work.append((remaining_time,
+                         new_ore - bp.geode[0], ore_bots,
+                         new_clay, clay_bots,
+                         new_obsidian - bp.geode[1], obsidian_bots,
+                         new_geode, geode_bots + 1))
+            continue  # we always build geode bots if we can
+
+        if new_ore < 22 or new_clay < 22 or new_obsidian < 22 or remaining_time > 2:
+            work.append((remaining_time,
+                         new_ore, ore_bots,
+                         new_clay, clay_bots,
+                         new_obsidian, obsidian_bots,
+                         new_geode, geode_bots))
+
+        if ore >= bp.ore:
+            work.append((remaining_time,
+                         new_ore - bp.ore, ore_bots + 1,
+                         new_clay, clay_bots,
+                         new_obsidian, obsidian_bots,
+                         new_geode, geode_bots))
+
+        if ore >= bp.clay:
+            work.append((remaining_time,
+                         new_ore - bp.clay, ore_bots,
+                         new_clay, clay_bots + 1,
+                         new_obsidian, obsidian_bots,
+                         new_geode, geode_bots))
+
+        if ore >= bp.obsidian[0] and clay >= bp.obsidian[1]:
+            work.append((remaining_time,
+                         new_ore - bp.obsidian[0], ore_bots,
+                         new_clay - bp.obsidian[1], clay_bots,
+                         new_obsidian, obsidian_bots + 1,
+                         new_geode, geode_bots))
 
     return best
 
 
-def max_geodes_facade(bp, n):
-    return max_geodes(bp, 0, 1, 0, 0, 0, 0, n)
-
-
 def part1(inp):
     blueprints = parse_input(inp)
-    qs = [bp.id * max_geodes_facade(bp, 24) for bp in blueprints]
+    qs = [bp.id * max_geodes(bp, 24) for bp in blueprints]
     print(qs)
     return sum(qs)
+
+
+N2 = 32
 
 
 def part2(inp):
     blueprints = parse_input(inp)[:3]
-    qs = [max_geodes_facade(bp, 36) for bp in blueprints]
+    qs = [max_geodes(bp, N2) for bp in blueprints]
     print(qs)
-    return sum(qs)
+    return qs[0] * qs[1] * qs[2]
 
 
-# assert part1(example) == 33
-# print("Part 1:", part1(task))
+assert part1(example) == 33
+print("Part 1:", part1(task))
 
-# assert part2(example) == 62
+assert max_geodes(parse_input(example)[0], N2) == 56
+assert max_geodes(parse_input(example)[1], N2) == 62
 print("Part 2:", part2(task))
